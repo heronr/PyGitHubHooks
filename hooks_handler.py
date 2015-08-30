2fd012b2e8cf75fb7a5cff0025b6fbb009ca7bd5
nrivera <heronr1@gmail.com>
2015-08-29 22:51:18 -0700

    Simple python web server that handles POST requests

---
+++ hooks_handler.py
@@ -0,0 +1,22 @@
+﻿import http.server
+import json
+
+class GitHubHooksHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        print('Got GET!')
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
+        self.end_headers()
+        return None
+        
+    def do_POST(self):
+        print('Got POST!')
+        #print(self.headers)
+        content_length = int(self.headers.get('content-length', 0))
+        if content_length > 0:
+            content = self.rfile.read(content_length)
+            print(content)
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
+        self.end_headers()
---
+++ server.py
@@ -0,0 +1,16 @@
+﻿import http.server
+from hooks_handler import GitHubHooksHandler
+
+
+handler = GitHubHooksHandler
+PORT = 8080
+host = '127.0.0.1'
+
+server = http.server.HTTPServer((host, PORT), handler)
+try:
+    server.serve_forever()
+except KeyboardInterrupt:
+    pass
+
+print('Done')
+
