43e5d2dc155dec92df768b326e1ca4993b86e2c8
nrivera <heronr1@gmail.com>
2015-08-30 00:49:58 -0700

    Better error messages that are sent as part of the response to the POST

--- hooks_handler.py
+++ hooks_handler.py
@@ -5,28 +5,31 @@ from git_hub_events import *
 
 class GitHubHooksHandler(http.server.BaseHTTPRequestHandler):
 
-    def finish_response(self, response_code):
+    def finish_response(self, response_code, error_msg=''):
         self.send_response(response_code)
         self.send_header('Content-Type', 'text/html')
         self.end_headers()
+        if len(error_msg):
+            print(error_msg)
+            self.wfile.write("<html><body><p>{}</p></body></html>".format(error_msg).encode('utf8'))
 
     #
     # Handler for the post request
     #     
     def do_POST(self):
         content_length = int(self.headers.get('content-length', 0))
-        event_type = self.headers.get('X-Github-Event', '')
-        if len(event_type) == 0:
-            self.finish_response(http.client.BAD_REQUEST)
+        event_str = self.headers.get('X-Github-Event', '')
+        if len(event_str) == 0:
+            self.finish_response(http.client.BAD_REQUEST, 'Request has no Github event type')
             return
 
-        real_type = GetEventType(event_type)
-        if real_type == None:
-            self.finish_response(http.client.BAD_REQUEST)
+        event_type = GetEventType(event_str)
+        if event_type == None:
+            self.finish_response(http.client.BAD_REQUEST, 'Unable to handle {} event'.format(event_str))
             return
 
         if content_length <= 0:
-            self.finish_response(http.client.NO_CONTENT)
+            self.finish_response(http.client.NO_CONTENT, 'Event {} had no post data'.format(event_str))
             return
 
         content = self.rfile.read(content_length)
@@ -36,9 +39,9 @@ class GitHubHooksHandler(http.server.BaseHTTPRequestHandler):
             'push' : HandlePushEvent,
             'ping' : HandlePingEvent,
         }
-        method = switcher.get(real_type.name, lambda x: False)
+        method = switcher.get(event_type.name, lambda x: False)
         method_result = method(json_object)
         if method_result:
             self.finish_response(http.client.OK)
         else:
-            self.finish_response(http.client.BAD_REQUEST)
+            self.finish_response(http.client.BAD_REQUEST, 'Event {} failed'.format(event_str))
