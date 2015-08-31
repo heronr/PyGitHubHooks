5405e04709f8637b62854f6413cfb8d0a63f6ca5
nrivera <heronr1@gmail.com>
2015-08-30 18:02:43 -0700

    Server now allows for specifying a host address and port

--- merge_server.py
+++ merge_server.py
@@ -1,10 +1,15 @@
 ﻿import http.server
+import argparse
 from hooks_handler import GitHubHooksHandler
 
+parser = argparse.ArgumentParser()
+parser.add_argument('host', nargs='?', default='127.0.0.1', help='The ip address to host the merge_server at.')
+parser.add_argument('port', nargs='?', type=int, default=8080, help='The port to host the merge_server at.')
+args = parser.parse_args()
 
 handler = GitHubHooksHandler
-PORT = 8080
-host = '127.0.0.1'
+PORT = args.port
+host = args.host
 
 server = http.server.HTTPServer((host, PORT), handler)
 try:
